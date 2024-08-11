eeb510d807b80409037542c7ba5a5801d7d72d01
Dipesh Pardeshi <152983518+dipeshpardeshi04@users.noreply.github.com>
2024-08-12 02:27:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,9 @@ let isFileReady = false; // Flag to indicate when the PDF is ready
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
-
+app.use(cors({
+  origin: 'https://chatopdf.netlify.app' // Replace with your Netlify domain
+}));
 // MongoDB connection
 require('dotenv').config();
 
